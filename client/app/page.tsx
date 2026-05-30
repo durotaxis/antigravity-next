e0d77b965f8b487d199110534f1ab2da3f4d3d91
durotaxis <yuji.kurosu@gmail.com>
2026-05-30 17:05:37 +0900

    Set new screen chart filter default to one month

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -52,7 +52,7 @@ type ApiRun = Partial<Run> & {
 
 const getDefaultChartStartDate = () => {
   const date = new Date();
-  date.setMonth(date.getMonth() - 2);
+  date.setMonth(date.getMonth() - 1);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
